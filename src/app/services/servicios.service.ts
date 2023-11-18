957e6883e95d1d0d277e694d760c3418d369108c
GerardoRodr <gerardorodriguezmonzon@gmail.com>
2023-11-18 10:48:30 -0500

    Añadido services/servicios

---
+++ src/app/services/servicios.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiciosService {
+
+  constructor() { }
+
+  crearServicio(servicio: object) {
+    
+  }
+}
