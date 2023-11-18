9e7a01bc10851becbea991fe7ba2713b0952832c
GerardoRodr <gerardorodriguezmonzon@gmail.com>
2023-11-18 14:16:55 -0500

    Terminado componente inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
